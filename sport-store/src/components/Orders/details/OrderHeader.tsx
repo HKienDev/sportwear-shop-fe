33fd78fe25c7cbbbe5b1168f92cff23dad2bcec4
HKienDev <caythuemw1@gmail.com>
2025-03-14 21:33:58 +0700

    Move File + Done UI Order detail

--- sport-store/src/components/Orders/details/OrderHeader.tsx
+++ sport-store/src/components/Orders/details/OrderHeader.tsx
@@ -16,7 +16,7 @@ interface OrderHeaderProps {
     return (
       <div className="mb-6">
         <div className="flex items-center space-x-2 mt-2">
-          <span className="text-blue-600 font-semibold">ĐƠN HÀNG {orderId}</span>
+          <span className="text-blue-600 font-semibold">ĐƠN HÀNG: {orderId}</span>
           <span className="bg-yellow-100 text-yellow-600 px-2 py-1 text-sm rounded">
             {status}
           </span>
