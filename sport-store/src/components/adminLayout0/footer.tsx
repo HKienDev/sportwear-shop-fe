e7d7d60d55a261ec5ea1c0d39a3daf6f54c13055
HKienDev <caythuemw1@gmail.com>
2025-03-16 10:59:31 +0700

    fix folder

--- sport-store/src/components/adminLayout0/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="w-full bg-white py-4 border-t border-gray-300 text-center bottom-0 left-0">
-        <p className="text-sm text-gray-600">
-          © 2025 <span className="font-semibold">Vju Sport</span> | All Rights Reserved.
-        </p>
-      </footer>
-    );
-  }
