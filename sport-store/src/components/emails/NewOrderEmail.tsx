3690387948937c50b216343d6fc552972970a376
HKienDev <caythuemw1@gmail.com>
2025-05-14 03:36:54 +0700

    update

--- sport-store/src/components/emails/NewOrderEmail.tsx
+++ sport-store/src/components/emails/NewOrderEmail.tsx
@@ -1,29 +1,5 @@
 import React from 'react';
-
-export interface OrderEmailProps {
-  shortId: string;
-  items: Array<{
-    name: string;
-    quantity: number;
-    price: number;
-  }>;
-  subtotal: number;
-  directDiscount: number;
-  couponDiscount: number;
-  shippingFee: number;
-  totalPrice: number;
-  shippingAddress: {
-    fullName: string;
-    phone: string;
-    address: {
-      street: string;
-      ward: { name: string };
-      district: { name: string };
-      province: { name: string };
-    };
-  };
-  createdAt: string;
-}
+import { OrderEmailProps } from '@/types/email';
 
 export const NewOrderEmail: React.FC<OrderEmailProps> = (order) => {
   return (
