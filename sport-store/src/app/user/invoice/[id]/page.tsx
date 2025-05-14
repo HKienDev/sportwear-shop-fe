79f40421d2cdf62a86aa9f78e2d730972a0b6332
HKienDev <caythuemw1@gmail.com>
2025-05-14 14:29:21 +0700

    update performan web

--- sport-store/src/app/user/invoice/[id]/page.tsx
+++ sport-store/src/app/user/invoice/[id]/page.tsx
@@ -164,7 +164,7 @@ export default function InvoicePage() {
         };
       })
     );
-    return processedItems.filter(Boolean);
+    return processedItems.filter((item): item is ProcessedProduct => !!item);
   }, []);
 
   useEffect(() => {
