cafaef562c159822e409461a0bbe61dcd1d33559
HKienDev <caythuemw1@gmail.com>
2025-05-14 11:29:51 +0700

    update performan web

--- sport-store/src/app/user/head.tsx
+++ sport-store/src/app/user/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <link rel="preload" as="image" href="/Ronaldo.png" imagesrcset="/Ronaldo.png 1x, /Ronaldo@2x.png 2x" imagesizes="100vw" />
+      <link rel="preload" as="image" href="/Ronaldo.png" />
     </>
   );
 } 
