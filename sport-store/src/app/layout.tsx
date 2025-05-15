5d53fc0ca319933804bd1abf93ad8467920212b4
HKienDev <caythuemw1@gmail.com>
2025-05-15 13:01:15 +0700

    update performan web

--- sport-store/src/app/layout.tsx
+++ sport-store/src/app/layout.tsx
@@ -1,1 +1,39 @@
-g
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import ClientLayout from "@/components/ClientLayout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
+
+const montserrat = Montserrat({ 
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat"
+});
+
+export const metadata: Metadata = {
+  title: "Sport Store",
+  description: "Cửa hàng thể thao trực tuyến",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Đã dùng next/font nên không cần preload Google Fonts ở đây */}
+        <link rel="icon" href="vju-logo-main.png" type="image/png" />
+      </head>
+      <body className={`${montserrat.variable} font-sans min-h-screen bg-background antialiased`}>
+        <ClientLayout fontClass={montserrat.variable}>
+                    {children}
+        </ClientLayout>
+        <SpeedInsights />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
