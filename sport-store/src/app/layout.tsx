3194acd866b57cd033c14c8c5f3f3eb2efe1cb3c
HKienDev <caythuemw1@gmail.com>
2025-07-15 00:23:16 +0700

    chore: remove Vercel Analytics component from app layout temporarily

--- sport-store/src/app/layout.tsx
+++ sport-store/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ClientLayout from "@/components/ClientLayout";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Sport Store",
@@ -24,7 +23,6 @@ export default function RootLayout({
           {children}
         </ClientLayout>
         <SpeedInsights />
-        <Analytics />
       </body>
     </html>
   );
